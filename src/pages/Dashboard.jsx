0d01c3433046b3e954b7c744bdab330575e9e561
Overmaths <joacdele19@gmail.com>
2026-09-11 00:48:52 +0100

    Fix dashboard API import path

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { supabase } from '../supabaseClient'
-import { getDashboardSummary } from './dashboard/dashboardApi'
+import { getDashboardSummary } from '../dashboardApi'
 import './Dashboard.css'
 
 function Dashboard() {
